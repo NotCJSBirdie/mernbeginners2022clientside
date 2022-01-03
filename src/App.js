facbbc225eb40f4f35d7173ce449eee8eb268bb7
Carl Serquiña <serquinajhunlo@gmail.com>
2022-01-03 16:58:06 +0800

    version2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [listofUsers, setListofUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    Axios.get("https://mern-beginners-2022.herokuapp.com/getUsers").then(
+      (response) => {
+        setListofUsers(response.data);
+      }
+    );
+  }, []);
+
+  const createUser = () => {
+    Axios.post("https://mern-beginners-2022.herokuapp.com/createUsers", {
+      name: name,
+      age: age,
+      username: username,
+    }).then((response) => {
+      alert("USER CREATED");
+      setListofUsers([
+        ...listofUsers,
+        {
+          name: name,
+          age: age,
+          username: username,
+        },
+      ]);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to React FULL STACK!</h1>
+
+      <div>
+        {listofUsers.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Username: {user.username}</h1>
+            </div>
+          );
+        })}
+      </div>
+      <input
+        type="text"
+        placeholder="name"
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="age"
+        onChange={(event) => {
+          setAge(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }}
+      />
+      <button onClick={createUser}>Create User</button>
+      <div></div>
     </div>
   );
 }
